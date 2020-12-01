2d571822bd763ed91c566ee1d7782c18d4a31c10
Ben Myers <ben.myers@okstate.edu>
2020-12-01 10:32:38 -0600

    Day 1 - Part 2

--- 01/index.js
+++ 01/index.js
@@ -16,3 +16,17 @@ for (let i = 0; i < lines.length - 1; i++) {
 		}
 	}
 }
+
+for (let i = 0; i < lines.length - 2; i++) {
+	for (let j = i + 1; j < lines.length - 1; j++) {
+		for (let k = j + 1; k < lines.length; k++)
+		if (lines[i] + lines[j] + lines[k] === 2020) {
+			console.table({
+				i: lines[i],
+				j: lines[j],
+				k: lines[k],
+				product: lines[i] * lines[j] * lines[k]
+			});
+		}
+	}
+}
