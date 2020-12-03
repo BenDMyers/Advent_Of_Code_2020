93ba8e842e68c3cbd562ea940eb71243e4219d64
Ben Myers <ben.myers@okstate.edu>
2020-12-02 23:09:43 -0600

    Day 3 - Part 2

--- 03/index.js
+++ 03/index.js
@@ -18,3 +18,28 @@ while (y < grid.length) {
 }
 
 console.log(encounters);
+
+// Part 2
+function testSlope(xOffset, yOffset) {
+	let x = 0, y = 0;
+	let encounters = 0;
+
+	while (y < grid.length) {
+		if (grid[y][x] === '#') {
+			encounters++
+		}
+
+		x = (x + xOffset) % grid[0].length;
+		y += yOffset;
+	}
+
+	return encounters;
+}
+
+const firstTest = testSlope(1, 1);
+const secondTest = testSlope(3, 1);
+const thirdTest = testSlope(5, 1);
+const fourthTest = testSlope(7, 1);
+const fifthTest = testSlope(1, 2);
+
+console.log(firstTest * secondTest * thirdTest * fourthTest * fifthTest);
