24a410cadc7a34ecd8b5327eb48c1fa15a25da20
Ben Myers <ben.myers@okstate.edu>
2020-12-06 07:52:11 -0600

    Day 6 - Part 2

--- 06/index.js
+++ 06/index.js
@@ -11,3 +11,28 @@ const groupUniqueAnswerSum = groups.reduce((sum, group) => {
 }, 0);
 
 console.log(groupUniqueAnswerSum);
+
+// Part 2
+/**
+ * Gets the intersection of two `Set`s
+ * @param {Set<String>} a first `Set`
+ * @param {Set<String>} b second `Set`
+ * @returns {Set<String>} intersection of `a` and `b` (a `Set` of every element in both `a` and `b`)
+ */
+function intersection(a, b) {
+	return new Set([...a].filter(aItem => b.has(aItem)));
+}
+
+const groupUnanimousAnswerSum = groups.reduce((sum, group) => {
+	const allAnswers = group.split('\n');
+	const firstSetOfAnswers = new Set(allAnswers[0].split(''));
+
+	const unanimousAnswers = allAnswers.reduce((consensus, answers) => {
+		const uniqueAnswers = new Set(answers.split(''));
+		return intersection(consensus, uniqueAnswers);
+	}, firstSetOfAnswers);
+
+	return sum + unanimousAnswers.size;
+}, 0);
+
+console.log(groupUnanimousAnswerSum);
