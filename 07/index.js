6b50c8e48cb92dbd687aff682686cf2050cf486b
Ben Myers <ben.myers@okstate.edu>
2020-12-06 23:56:04 -0600

    Day 7 - Part 2

--- 07/index.js
+++ 07/index.js
@@ -20,7 +20,6 @@ for (const rule of rules) {
 			allowedNesting[outerBag].push(matches[1]);
 		}
 	}
-
 }
 
 // Traverse all provided colors, and figure out whether they can eventually carry shiny gold
@@ -50,3 +49,42 @@ console.log(memoized)
 
 // Count up how many starting bags could carry shiny gold
 console.log(Object.values(memoized).filter(memo => memo === true).length);
+
+////// Part 2
+
+// Parse how many of each bag goes in any particular bag
+const nestedBagCounts = {};
+for (const rule of rules) {
+	const [outerBag, allInnerBags] = rule.split(' bags contain ');
+	if (!nestedBagCounts[outerBag]) {
+		nestedBagCounts[outerBag] = [];
+	}
+
+	allInnerBags
+		.split(/\sbag[s,.\s]*/) // split the list of nested colors into just the count and the color name
+		.filter(s => s) // remove any empty elements
+		.forEach(bagCount => {
+			const matches = bagCount.match(/(\d+)\s(.*)/);
+			if (matches) {
+				const [, count, color] = matches;
+				nestedBagCounts[outerBag].push({color, count});
+			}
+		});
+}
+
+const memoizedBagCounts = {};
+function getNumberOfNestedBags(color) {
+	if (memoizedBagCounts[color] !== undefined) {
+		return memoizedBagCounts[color];
+	}
+
+	let count = 0;
+	for (let nested of nestedBagCounts[color]) {
+		count += (nested.count * getNumberOfNestedBags(nested.color));
+	}
+
+	return count + 1;
+}
+
+const totalBags = getNumberOfNestedBags('shiny gold') - 1;
+console.log(totalBags)
