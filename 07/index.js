5d09fabfce4582888854ebd67629bc9309b695a1
Ben Myers <ben.myers@okstate.edu>
2020-12-07 17:27:00 -0600

    Day 7 - Refactor input parsing

--- 07/index.js
+++ 07/index.js
@@ -1,90 +1,86 @@
 const fs = require('fs');
 const rules = fs
 	.readFileSync(`${__dirname}/.input`, 'utf-8')
-	.split('\n')
+	.split('\n');
 
-////// Part 1
-const innerBagRegex = /[\d\s]+(?<color>[\w\s]+) bag[s]*/g;
+// Assemble tree with given rules
+const outerBagRegex = /([\w\s]+\w) bags contain/;
+const innerBagRegex = /(?<count>\d+) (?<color>[\w\s]+\w) bags?[,.]/g;
 
-// Parse the provided rules - figure out which bags can go in which other bags
-const allowedNesting = {};
-for (const rule of rules) {
-	const [outerBag, allInnerBags] = rule.split(' bags contain ');
-	if (!allowedNesting[outerBag]) {
-		allowedNesting[outerBag] = [];
-	}
+const nestingRules = {};
 
-	if (allInnerBags !== 'no other bags.') {
-		let matches;
-		while (matches = innerBagRegex.exec(allInnerBags)) {
-			allowedNesting[outerBag].push(matches[1]);
-		}
+for (let rule of rules) {
+	const outerBag = rule.match(outerBagRegex)[1];
+	nestingRules[outerBag] = [];
+
+	const innerBagMatches = rule.matchAll(innerBagRegex);
+	for (let match of innerBagMatches) {
+		const {count, color} = match.groups;
+		nestingRules[outerBag].push({count, color});
 	}
 }
 
-// Traverse all provided colors, and figure out whether they can eventually carry shiny gold
-const memoized = {};
-function canContainShinyGoldBag(color) {
-	if (memoized[color] !== undefined) {
-		return memoized[color];
+const allColors = Object.keys(nestingRules);
+
+
+// Part 1: Determine whether a given bag color can eventually contain a shiny gold bag
+
+/** @type {Object<string, boolean>} */
+const containsShinyGoldMemoized = {};
+
+/**
+ * Get whether a shiny gold bag could be nested inside a bag of the given color [memoized]
+ * @param {string} color bag color
+ * @returns {boolean} whether this bag could contain a shiny gold bag
+ */
+function containsShinyGold(color) {
+	// Leverage memoization
+	if (containsShinyGoldMemoized[color] !== undefined) {
+		return containsShinyGoldMemoized[color];
 	}
 
-	if (allowedNesting[color].includes('shiny gold')) {
-		memoized[color] = true;
-		return true;
-	}
-
-	for (let i = 0; i < allowedNesting[color].length; i++) {
-		if (canContainShinyGoldBag(allowedNesting[color][i])) {
-			memoized[color] = true;
+	const nestedBags = nestingRules[color];
+	for (let bag of nestedBags) {
+		if (bag.color === 'shiny gold' || containsShinyGold(bag.color)) {
+			containsShinyGoldMemoized[color] = true;
 			return true;
 		}
 	}
 
+	containsShinyGoldMemoized[color] = false;
 	return false;
 }
 
-Object.keys(allowedNesting).forEach(canContainShinyGoldBag);
-console.log(memoized)
+const allowedNestingCount = allColors.reduce(
+	(count, color) => containsShinyGold(color) ? (count + 1) : count, 
+	0
+);
+console.log(allowedNestingCount);
 
-// Count up how many starting bags could carry shiny gold
-console.log(Object.values(memoized).filter(memo => memo === true).length);
 
-////// Part 2
 
-// Parse how many of each bag goes in any particular bag
-const nestedBagCounts = {};
-for (const rule of rules) {
-	const [outerBag, allInnerBags] = rule.split(' bags contain ');
-	if (!nestedBagCounts[outerBag]) {
-		nestedBagCounts[outerBag] = [];
+// Part 2: Determine how many bags can be found inside a single shiny gold bag
+
+/** @type {Object<string, number>} */
+const countNestedBagsMemoized = {};
+
+/**
+ * Gets the total number of bags nested inside the provided color [memoized]
+ * @param {string} color bag color
+ * @returns {number} count of bags nested inside one bag of the provided color
+ */
+function countNestedBags(color) {
+	if (countNestedBagsMemoized[color] !== undefined) {
+		return countNestedBagsMemoized[color];
 	}
 
-	allInnerBags
-		.split(/\sbag[s,.\s]*/) // split the list of nested colors into just the count and the color name
-		.filter(s => s) // remove any empty elements
-		.forEach(bagCount => {
-			const matches = bagCount.match(/(\d+)\s(.*)/);
-			if (matches) {
-				const [, count, color] = matches;
-				nestedBagCounts[outerBag].push({color, count});
-			}
-		});
+	const nestedBags = nestingRules[color];
+	const nestedBagCount = nestedBags.reduce((count, bag) => {
+		return count + (bag.count * (1 + countNestedBags(bag.color)));
+	}, 0);
+
+	countNestedBagsMemoized[color] = nestedBagCount;
+	return nestedBagCount;
 }
 
-const memoizedBagCounts = {};
-function getNumberOfNestedBags(color) {
-	if (memoizedBagCounts[color] !== undefined) {
-		return memoizedBagCounts[color];
-	}
-
-	let count = 0;
-	for (let nested of nestedBagCounts[color]) {
-		count += (nested.count * getNumberOfNestedBags(nested.color));
-	}
-
-	return count + 1;
-}
-
-const totalBags = getNumberOfNestedBags('shiny gold') - 1;
-console.log(totalBags)
+console.log(countNestedBags('shiny gold'));
