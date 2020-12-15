74578e238cfe816a28740f03d7ded26cc0f9beb5
Ben Myers <ben.myers@okstate.edu>
2020-12-15 10:32:25 -0600

    Day 15 - Part 2

--- 15/index.js
+++ 15/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const input = fs
 	.readFileSync(`${__dirname}/.input`, 'utf-8')
-	.split(',');
+	.split(',')
+	.map(num => Number(num));
 
 // Part 1
 (function part1() {
@@ -35,5 +36,30 @@ const input = fs
 
 // Part 2
 (function part2() {
-	
+	/** @type {Map<number, number>} */
+	const spoken = new Map();
+
+	let previousNumber = 0;
+	let turn = 0;
+
+	// Initialize with provided input
+	for (; turn < input.length; turn++) {
+		spoken.set(input[turn], turn);
+		previousNumber = input[turn];
+	}
+
+	// Keep going
+	for (; turn < 30000000; turn++) {
+		let previousNumberLastSpoken = spoken.get(previousNumber);
+		spoken.set(previousNumber, turn - 1);
+
+		if (previousNumberLastSpoken >= 0) {
+			let age = turn - 1 - previousNumberLastSpoken;
+			previousNumber = age;
+		} else {
+			previousNumber = 0;
+		}
+	}
+
+	console.log(previousNumber);
 })();
