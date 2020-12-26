1d7d5fa49af64132bdace00644e994e795195e63
Ben Myers <ben.myers@okstate.edu>
2020-12-25 23:08:45 -0600

    Day 21 - Part 2

--- 21b/index.js
+++ 21b/index.js
@@ -1,3 +1,4 @@
+const {comparators} = require('generate-comparators');
 const fs = require('fs');
 const input = fs
 	.readFileSync(`${__dirname}/.input`, 'utf-8')
@@ -54,4 +55,52 @@ const input = fs
 	let nonallergenicCount = input.reduce((sum, {ingredients}) => sum + ingredients.filter(ingredient => nonallergenicIngredients.has(ingredient)).length, 0);
 
 	console.log(nonallergenicCount);
+})();
+
+
+
+(function part2() {
+	const byAllergen = comparators(([allergen]) => allergen);
+
+	/** @type {Set<string>} */
+	let allAllergens = new Set();
+	/** @type {Set<string>} */
+	let allIngredients = new Set();
+
+	input.forEach(({allergens, ingredients}) => {
+		allergens.forEach(allergen => allAllergens.add(allergen));
+		ingredients.forEach(ingredient => allIngredients.add(ingredient));
+	});
+
+	// In the beginning, any allergen can map to any ingredient
+	let allergenIngredientOptions = [...allAllergens].reduce((options, allergen) => {
+		options[allergen] = [...allIngredients];
+		return options;
+	}, {});
+
+	// If an allergen is explicitly listed, one of the listed ingredients HAS to have that allergen, so filter anything else out
+	input.forEach(({allergens, ingredients}) => {
+		allergens.forEach(allergen => {
+			allergenIngredientOptions[allergen] = allergenIngredientOptions[allergen].filter(ingredient => ingredients.includes(ingredient));
+		});
+	});
+
+	// Iteratively filter out ingredients that have already been locked in for other allergens
+	while (Object.values(allergenIngredientOptions).some(options => options.length > 1)) {
+		let lockedIn = Object.values(allergenIngredientOptions).filter(options => options.length === 1).flat();
+		console.log(lockedIn)
+		allAllergens.forEach((allergen) => {
+			if (allergenIngredientOptions[allergen].length > 1) {
+				allergenIngredientOptions[allergen] = allergenIngredientOptions[allergen].filter(ingredient => !lockedIn.includes(ingredient));
+			}
+		});
+	}
+
+	// Generate comma-separated string of dangerous ingredients
+	let dangerousIngredientList = Object.entries(allergenIngredientOptions)
+		.sort(byAllergen.asc)
+		.map(([, ingredient]) => ingredient)
+		.join(',');
+
+	console.log(dangerousIngredientList);
 })();
